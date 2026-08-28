16ab095a40ce0642b4cb9070605984c281acdc5a
Mitch2013 <mdholt@ualr.edu>
2026-08-28 20:58:30 +0000

    Reattempt submission

--- hello_world.py
+++ hello_world.py
@@ -4,3 +4,4 @@ print('Hi, how are you? My name is Mitchell Holt.')
 print('My favorite color is purple')
 print("Son:'My father always gives the best advice.' Father:'Son, if at first you don't succeed...destroy all of the evidence of you trying and don't look back.'")
 # Always test your code before submitting.
+# Not sure if it synced, so this is a reattempt.
