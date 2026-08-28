e0da759a895b8a2a1d2f5322d9fa6ef33dd5b963
Mitch2013 <mdholt@ualr.edu>
2026-08-28 20:45:42 +0000

    Assignment 1 attempt 1

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
-print("hello world")
+# AI Usage: None. I wrote this code myself.
+# I do not understand how this is applied in the real world.
+print('Hi, how are you? My name is Mitchell Holt.')
+print('My favorite color is purple')
+print("Son:'My father always gives the best advice.' Father:'Son, if at first you don't succeed...destroy all of the evidence of you trying and don't look back.'")
+# Always test your code before submitting.
