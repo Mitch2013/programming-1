410cc048cb47fe0b16b4c37977af0d29f61e60ac
Mitch2013 <mdholt@ualr.edu>
2026-08-29 22:47:25 +0000

    quick fix

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,8 @@
+#Mitchell Holt
+#Programming 1
+
 # AI Usage: None. I wrote this code myself.
-# I do not understand how this is applied in the real world.
+# I have alot to learn.
 print('Hi, how are you? My name is Mitchell Holt.')
 print('My favorite color is purple')
 print("Son:'My father always gives the best advice.' Father:'Son, if at first you don't succeed...destroy all of the evidence of you trying and don't look back.'")
